38a59342320f7486863a6bc15809ae6ee779494e
Shayne Boyer <spboyer@live.com>
2018-06-28 18:21:51 -0400

    Adding spinner and maikng async proper

--- src/Program.cs
+++ src/Program.cs
@@ -4,34 +4,45 @@ using McMaster.Extensions.CommandLineUtils;
 using System.Collections.Generic;
 using System.Net.Http;
 using Newtonsoft.Json;
+using Kurukuru;
+using System.Threading.Tasks;
 
 namespace dotnetcowsay
 {
-    [Command(Description = "My global command line tool.")]
-    class Program
+  [Command(Description = "My global command line tool.")]
+  class Program
+  {
+    public static int Main(string[] args) => CommandLineApplication.Execute<Program>(args);
+
+    private async Task<int> OnExecute()
     {
-        public static int Main(string[] args) => CommandLineApplication.Execute<Program>(args);
+      await GetLink();
 
-        private int OnExecute()
-        {
-            var cow = Cow.GetCow();
-            HttpClient client = new HttpClient();
-            string url = "https://discoverdot.net/data/blogs.json";
-            HttpResponseMessage response = client.GetAsync(url).Result;
-            string content = response.Content.ReadAsStringAsync().Result;
-
-            var blogList = JsonConvert.DeserializeObject<List<Blog>>(content);
-
-
-            Random rnd = new Random();
-            var blog = blogList[rnd.Next(0, blogList.Count - 1)];
-
-
-            var bubbleText = blog.title + Environment.NewLine + blog.newestFeedItem.title + Environment.NewLine + blog.newestFeedItem.link;
-            string SpeechBubbleReturned = SpeechBubble.ReturnSpeechBubble(bubbleText, new SayBubbleChars(), blog.newestFeedItem.link.Length);
-            Console.WriteLine(SpeechBubbleReturned + Environment.NewLine + cow);
-
-            return 0;
-        }
+      return 0;
     }
+
+    private async Task GetLink()
+    {
+      await Spinner.StartAsync("Looking for an awesome article..", async spinner =>
+      {
+        Blog blog = new Blog();
+
+        HttpClient client = new HttpClient();
+        string url = "https://discoverdot.net/data/blogs.json";
+        string content = await client.GetStringAsync(url);
+
+        var blogList = JsonConvert.DeserializeObject<List<Blog>>(content);
+        Random rnd = new Random();
+        blog = blogList[rnd.Next(0, blogList.Count - 1)];
+
+        var cow = Cow.GetCow();
+        var bubbleText = blog.title + Environment.NewLine + blog.newestFeedItem.title + Environment.NewLine + blog.newestFeedItem.link;
+        string SpeechBubbleReturned = SpeechBubble.ReturnSpeechBubble(bubbleText, new SayBubbleChars(), blog.newestFeedItem.link.Length);
+        Console.WriteLine(Environment.NewLine + SpeechBubbleReturned + Environment.NewLine + cow);
+
+        spinner.Succeed("Enjoy!");
+        //return blog;
+      });
+    }
+  }
 }
