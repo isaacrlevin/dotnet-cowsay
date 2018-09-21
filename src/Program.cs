99d439217eb4caa674b3a3d726ac9d4d6ff74866
Isaac Levin <isaac.r.levin@gmail.com>
2018-09-21 12:41:24 -0400

    Added Error Handling

--- src/Program.cs
+++ src/Program.cs
@@ -9,40 +9,45 @@ using System.Threading.Tasks;
 
 namespace dotnetcowsay
 {
-  [Command(Description = "My global command line tool.")]
-  class Program
-  {
-    public static int Main(string[] args) => CommandLineApplication.Execute<Program>(args);
-
-    private async Task<int> OnExecute()
+    [Command(Description = "Cowsay a Blog Post.")]
+    class Program
     {
-      await GetLink();
+        public static int Main(string[] args) => CommandLineApplication.Execute<Program>(args);
 
-      return 0;
+        private async Task<int> OnExecute()
+        {
+            await GetLink();
+
+            return 0;
+        }
+
+        private async Task GetLink()
+        {
+            await Spinner.StartAsync("Looking for an awesome article..", async spinner =>
+            {
+                try
+                {
+                    Blog blog = new Blog();
+
+                    HttpClient client = new HttpClient();
+                    string url = "https://discoverdot.net/data/blogs.json";
+                    string content = await client.GetStringAsync(url);
+
+                    var blogList = JsonConvert.DeserializeObject<List<Blog>>(content);
+                    Random rnd = new Random();
+                    blog = blogList[rnd.Next(0, blogList.Count - 1)];
+
+                    var cow = Cow.GetCow();
+                    var bubbleText = blog.title + Environment.NewLine + blog.newestFeedItem.title + Environment.NewLine + blog.newestFeedItem.link;
+                    string SpeechBubbleReturned = SpeechBubble.ReturnSpeechBubble(bubbleText, new SayBubbleChars(), blog.newestFeedItem.link.Length);
+                    Console.WriteLine(Environment.NewLine + SpeechBubbleReturned + Environment.NewLine + cow);
+                    spinner.Succeed("");
+                }
+                catch
+                {
+                    spinner.Fail("Something went wrong, please try again");
+                }
+            });
+        }
     }
-
-    private async Task GetLink()
-    {
-      await Spinner.StartAsync("Looking for an awesome article..", async spinner =>
-      {
-        Blog blog = new Blog();
-
-        HttpClient client = new HttpClient();
-        string url = "https://discoverdot.net/data/blogs.json";
-        string content = await client.GetStringAsync(url);
-
-        var blogList = JsonConvert.DeserializeObject<List<Blog>>(content);
-        Random rnd = new Random();
-        blog = blogList[rnd.Next(0, blogList.Count - 1)];
-
-        var cow = Cow.GetCow();
-        var bubbleText = blog.title + Environment.NewLine + blog.newestFeedItem.title + Environment.NewLine + blog.newestFeedItem.link;
-        string SpeechBubbleReturned = SpeechBubble.ReturnSpeechBubble(bubbleText, new SayBubbleChars(), blog.newestFeedItem.link.Length);
-        Console.WriteLine(Environment.NewLine + SpeechBubbleReturned + Environment.NewLine + cow);
-
-        spinner.Succeed("Enjoy!");
-        //return blog;
-      });
-    }
-  }
 }
